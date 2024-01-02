049cb4a9ea6cfd454c8e0bc2f99e722fcc45b090
Arun <arun@arun.blog>
2024-01-02 15:29:17 -0800

    Refactor to use map for planet ratios

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -2,25 +2,20 @@ package space
 
 type Planet string
 
+var planetRatios = map[Planet]float64{
+	"Earth":   1,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
 func Age(seconds float64, planet Planet) float64 {
-	switch planet {
-	case "Earth":
-		return seconds / 31557600
-	case "Mercury":
-		return seconds / (0.2408467 / 1 * 31557600)
-	case "Venus":
-		return seconds / (0.61519726 / 1 * 31557600)
-	case "Mars":
-		return seconds / (1.8808158 / 1 * 31557600)
-	case "Jupiter":
-		return seconds / (11.862615 / 1 * 31557600)
-	case "Saturn":
-		return seconds / (29.447498 / 1 * 31557600)
-	case "Uranus":
-		return seconds / (84.016846 / 1 * 31557600)
-	case "Neptune":
-		return seconds / (164.79132 / 1 * 31557600)
-	default:
-		return -1
+	if ratio, ok := planetRatios[planet]; ok {
+		return seconds / (ratio * 31557600)
 	}
+	return -1
 }
